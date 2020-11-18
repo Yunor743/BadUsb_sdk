b6868d18fdf85df0735ad523bbddbedab0886ca1
Hugo PERINAZZO <hugo.perinazzo@epitech.eu>
2020-11-18 16:10:52 +0100

    trying to add extensions

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,25 @@
 #include <Keyboard.h>
 
 void setup() {
+
+// TODO determine the OS
+
   Keyboard.begin();
   delay(1000);
-  Keyboard.press(KEY_LEFT_GUI);
-  Keyboard.press('r');
-  Keyboard.releaseAll();
-  delay(150);
-  Keyboard.print("notepad.exe");
-  delay(150);
-  Keyboard.end();
+  Keyboard.press(CTRL);
+  Keyboard.press(LEFT_ALT);
+  Keyboard.press('t');
+
+
+  // Keyboard.begin();
+  // delay(1000);
+  // Keyboard.press(KEY_LEFT_GUI);
+  // Keyboard.press('r');
+  // Keyboard.releaseAll();
+  // delay(150);
+  // Keyboard.print("notepad.exe");
+  // delay(150);
+  // Keyboard.end();
   // put your setup code here, to run once:
 }
 
