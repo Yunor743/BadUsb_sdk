88ff552c292082482257cbbd0d16916fa3139680
Hugo PERINAZZO <hugo.perinazzo@epitech.eu>
2020-11-18 22:46:08 +0100

    working on architecture

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,30 @@
 #include <Arduino.h>
 #include <Keyboard.h>
 #include <iostream>
+#include <string>
+#include <vector>
 
-void setup() {
-
-// TODO determine the OS
-
-  Keyboard.begin();
-  delay(1000);
-  // delay(1000);
-  // Keyboard.press(CTRL);
-  // Keyboard.press(LEFT_ALT);
-  // Keyboard.press('t');
-
-
-  // Keyboard.begin();
-  // delay(1000);
-  // Keyboard.press(KEY_LEFT_GUI);
-  // Keyboard.press('r');
-  // Keyboard.releaseAll();
-  // delay(150);
-  // Keyboard.print("notepad.exe");
-  // delay(150);
-  // Keyboard.end();
-  // put your setup code here, to run once:
-  std::vector<int> vec;
-  vec.push_back(7);
-  vec.push_back(4);
-  vec.push_back(3);
-  vec.push_back(8);
-  vec.push_back(0);
-  for (auto i = 0; i < vec.size(); ++i) {
-    Keyboard.press('0' + i);
-    delay(500);
-    Keyboard.releaseAll();
-    delay(500);
-  }
+void setup()
+{
+    // TODO determine the OS
+    Serial.begin(9600);
+    Keyboard.begin();
+    delay(1000);
+    std::vector<int> vec;
+    vec.push_back(7);
+    vec.push_back(4);
+    vec.push_back(3);
+    vec.push_back(8);
+    vec.push_back(0);
+    delay(2000);
+    for (auto i = 0; i < vec.size(); ++i) {
+        Serial.println(i);
+    }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    // compil // TODO
+    // upload // sudo pio run -e leonardo -t upload
+    // serial // sudo pio device monitor
 }
